426a4f65dedc162b47de1e5074ca0edad107ef20
khetreJagdish <30476034+khetreJagdish@users.noreply.github.com>
2021-06-06 16:51:17 +0530

    Update ProductDAOImpl.java

--- REST_WS_Produce__Consume-master-master/src/main/java/com/scp/dao/ProductDAOImpl.java
+++ REST_WS_Produce__Consume-master-master/src/main/java/com/scp/dao/ProductDAOImpl.java
@@ -57,7 +57,7 @@ public class ProductDAOImpl implements ProductDAO {
 
 	@Override
 	public List<ProductEntity> getAllProducts() {
-		System.out.println("Inside getAllProducts() method MMMM");
+		System.out.println("Inside getAllProducts() method MMMMXS");
 		SessionFactory sessionFactory = SessionFactoryUtil.getSessionFactory();
 		Session session = sessionFactory.openSession();
 		List<ProductEntity> productEntities = session.createQuery("from ProductEntity").list();
